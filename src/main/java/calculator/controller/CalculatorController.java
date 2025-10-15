adf0967ccb480c034b6011c40636e68270cc6981
SoungJuyeon <tjdwndus1325@gmail.com>
2025-10-15 15:19:46 +0900

    feat: 사용자 입력 받아오기

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,16 @@
 package calculator.controller;
 
+import calculator.view.InputHandler;
+
 public class CalculatorController {
+    private InputHandler inputHandler;
+
+    public CalculatorController() {
+        inputHandler = new InputHandler();
+    }
+
+    public void start() {
+        String input = inputHandler.userInput();
+
+    }
 }
--- src/main/java/calculator/view/InputHandler.java
+++ src/main/java/calculator/view/InputHandler.java
@@ -1,4 +1,11 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputHandler {
+    private static final String USER_INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    public String userInput() {
+        System.out.println(USER_INPUT_MESSAGE);
+        return Console.readLine();
+    }
 }

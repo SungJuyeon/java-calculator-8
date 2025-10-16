140044f82a8a71798a79fa7403a07cb108c78e29
SoungJuyeon <tjdwndus1325@gmail.com>
2025-10-16 11:50:04 +0900

    test: 테스트 코드 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import calculator.domain.NumberValidator;
+import calculator.view.InputHandler;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,6 +28,27 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    @DisplayName("문자열 입력 시 예외")
+    void 문자열_입력_시_예외() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//*\\n1@2"))
+                .isInstanceOf(IllegalArgumentException.class)
+
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("다양한 커스텀 구분자 테스트")
+    @CsvSource(value = {"//*\\n1*2:3, 6", "//\\n1:2:5, 8"})
+    void 다양한_커스텀_구분자_테스트(String input, int expected) {
+        assertSimpleTest(() -> {
+            run(input);
+            assertThat(output()).contains("결과 : " + expected);
+        });
+    }
+
 
     @Override
     public void runMain() {
